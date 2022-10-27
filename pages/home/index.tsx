2ac66f1537776633c6a9411117d4dbfb4049ac0e
vechet <vechet240@gmail.com>
2022-10-27 12:02:11 +0700

    configure mui, styled-components, add home screen

---
+++ pages/home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Index from "../../src/views/Home/Index";
+
+const index = () => {
+  return <Index />;
+};
+
+export default index;
---
+++ src/views/Home/Index.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material";
+import React from "react";
+
+const Index = () => {
+  return (
+    <Stack justifyContent="center" alignItems="center" flex="1">
+      Currently, We are in develop our website.
+    </Stack>
+  );
+};
+
+export default Index;
