141609644e88c26e7ba74e546becffd317bb2cd9
vechet <vechet240@gmail.com>
2022-10-28 13:12:27 +0700

    update project type

---
+++ pages/projectType/[id]/index.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import React from "react";
+import Layout from "../../../src/components/Layout";
+import ProjectTypeCreate from "../../../src/view/ProjectType/ProjectTypeCreate";
+import ProjectTypeDetail from "../../../src/view/ProjectType/ProjectTypeDetail";
+const index = () => {
+  const router = useRouter();
+  const { query } = router;
+  const { id } = query;
+
+  return (
+    <Layout title="Ticket System">
+      {query.id !== "detail" && <ProjectTypeCreate />}
+      {query.id === "detail" && <ProjectTypeDetail />}
+    </Layout>
+  );
+};
+
+export default index;
---
+++ src/view/ProjectType/ProjectTypeDetail.tsx
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const ProjectTypeDetail = React.memo(() => {
+  return <div>ProjectTypeDetail</div>;
+});
+
+export default ProjectTypeDetail;
